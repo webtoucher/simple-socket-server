d6035bb55dc73bb60ca13994680d2ca6c9d86f4c
Алексей Кузнецов <mirakuru@webtoucher.ru>
2023-02-01 13:26:07 +0700

    Up version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='simple-socket-server',
-      version='1.1b',
+      version='1.1',
       url='https://github.com/webtoucher/simple-socket-server',
       license='BSD-3-Clause',
       author='Alexey Kuznetsov',
